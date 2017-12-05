7ea96a3d54ffe5c0075e5fe599a034cf0c29d491
Jacob Lang <langj123@gmail.com>
2017-12-05 16:58:42 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head lang="en" xml:lang="en">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>To Do List Creator</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <head>
+            <h1>To Do Lists <button id="To-Do-Generator">Add a To Do List</button></h1>
+        </head>
+        
+    </div>
+</body>
+</html>
