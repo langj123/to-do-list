57552476cb4cd07f8d287a87f62779cbb05fd5a0
Jacob Lang <langj123@gmail.com>
2017-12-07 10:30:34 -0500

    added captions to panel images

--- inc/db.php
+++ inc/db.php
@@ -1,7 +1,8 @@
 <?php
 	$db = new mysqli('localhost', 'root', 'root', 'testingdb');
 	if ($db->connect_error) {
-		die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+		// die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+		echo "not connected to db";
 	} else {
 		$db->close();
 	}
